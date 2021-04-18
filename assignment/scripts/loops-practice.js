2734de84700a1df0f13cebd629764cfb497bb2fc
Stephen Piper <stephen.r.piper@gmail.com>
2021-04-17 20:50:12 -0500

    percent is remainder question 1.c

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -21,10 +21,20 @@ for (let i = 0; i < 6; i++) {
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 3 to 5');
+for (let i = 3; i < 6; i++) {
+  console.log(i);
+}
 
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
+
+for (let i = 1; i < 11; i++) {
+  if ( i%2 === 0 ) {
+    console.log('Even numbers:',i);
+  }
+}
+
 
 // 1.d. (STRETCH) TODO: Write a for loop to do a counddown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
